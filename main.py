ea741be88609973c39df938dff212edaeb10101d
anastasiia3006 <108720682+anastasiia3006@users.noreply.github.com>
2025-02-06 11:42:04 +0100

    now

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
+from pandas.core.config_init import max_cols
 from sklearn.datasets import fetch_california_housing
 import folium
 from folium import plugins
@@ -8,9 +9,10 @@ from folium import plugins
 #- 1- visualisation price as color
 #- 2- visualisation size of the bubble
 
-data = fetch_california_housing(as_frame=True)
+#load data
+data = fetch_california_housing(as_frame=True).frame
 
-#exeplier map
+#create map
 m = folium.Map(location=[data['Latitude'].mean(), data['Longitude'].mean()], zoom_start=6)
 
 #make cheaper houses with green color and expensive house with red color
@@ -18,18 +20,32 @@ m = folium.Map(location=[data['Latitude'].mean(), data['Longitude'].mean()], zoo
 price_min, price_max = data['MedHouseVal'].min(), data['MedHouseVal'].max()
 size_min, size_max = data['AveRooms'].min(), data['AveRooms'].max()
 
+#prise normalisation
 for _, row in data.iterrows():
     normalized_prise = (row['MedHouseVal'] - price_min)/(price_max-price_min)
     # color come from green to red
     color = plt.cm.RdYlGn(1-normalized_prise)
 
+#normalisatoin of rooms
     normalized_rooms = (row['AveRooms'] - size_min)/(size_max-size_min)
+
+#deteminate color
     popup_info = f'''Median House Value: ${row['MedHouseVal']:.2f}<br>
     Average Rooms: {row['AveRooms']}<br>
     Population: {row['Population']}<br>
     Median Income: ${row['MedInc']:.2f}'''
 
+#create marker for points
     folium.CircleMarker(
         location = [row['Latitude'], row ['Longitude']],
-        radius=5+20
-    )
+        radius=5+20*normalized_rooms,
+        color = mcolors.to_hex(color[:3]),
+        fill=True,
+        fill_color = mcolors.to_hex(color[:3]),
+        fill_opacity=0.7,
+        popup=folium.Popup(popup_info, max_width=300)
+    ).add_to(m)
+
+# make mini-map
+plugins.MiniMap().add_to(m)
+m.save('real_state.html')
